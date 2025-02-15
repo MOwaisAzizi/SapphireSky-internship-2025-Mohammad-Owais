0b1bf5e2dace78430058ce5051487eadbceff9c8
owias azizi <owaisazizi360@gmail.com>
2025-02-15 15:49:58 +0430

    check if a number is even or odd.

--- Codding-Chalange/basic.js
+++ Codding-Chalange/basic.js
@@ -1,4 +1,14 @@
 
+function isOddOrEvenCheck(number){
+    if(number === 0) return 0
+    if(number % 2 === 0) return 'even'
+    else if(number % 2 !== 0) return 'odd'
+}
+
+ console.log(isOddOrEvenCheck(17));
+ console.log(isOddOrEvenCheck(120));
+ console.log(isOddOrEvenCheck(0));
+ 
 
 
 
