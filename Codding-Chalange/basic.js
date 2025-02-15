d4e3b71c542d4487f94ed13deb621ba18b70cbe3
owias azizi <owaisazizi360@gmail.com>
2025-02-15 16:02:40 +0430

    prints whether it is a positive, negative, or zero.

--- Codding-Chalange/basic.js
+++ Codding-Chalange/basic.js
@@ -1,14 +1,24 @@
+////pratical examples
 
+//Write a function to check if a number is even or odd.
 function isOddOrEvenCheck(number){
-    if(number === 0) return 0
-    if(number % 2 === 0) return 'even'
-    else if(number % 2 !== 0) return 'odd'
+    if(number === 0) return `${number} is zero!`
+    if(number % 2 === 0) return `${number} is even!`
+    else if(number % 2 !== 0) return `${number} is odd!`
 }
 
  console.log(isOddOrEvenCheck(17));
  console.log(isOddOrEvenCheck(120));
  console.log(isOddOrEvenCheck(0));
  
-
+ //Write a program that takes user input and prints whether it is a positive, negative, or zero.
+ function checkPositiveOrNegative(number){
+    if(number>0) return `${number} is a positive number!`
+    else if(number<0) return `${number} is a negative number!`
+    else if(number === 0) return `${number} is Zero!`
+   }
+    
+   const input = prompt('Enter A Number')
+   console.log(checkPositiveOrNegative(Number(input)));
 
 
