bd54508f2a08fe8d61375cc106a53ae6ebf95634
owias azizi <owaisazizi360@gmail.com>
2025-02-15 16:36:39 +0430

    determine their letter grade based on the following scale

--- Codding-Chalange/easy.js
+++ Codding-Chalange/easy.js
@@ -11,3 +11,25 @@ console.log(isOddOrEvenCheck(17));
 console.log(isOddOrEvenCheck(120));
 console.log(isOddOrEvenCheck(0));
 
+
+//Given a student's score (0-100), determine their letter grade based on the following scale:
+// 90-100: A
+// 80-89: B
+// 70-79: C
+// 60-69: D
+// Below 60: F
+
+function checkStudentGrade(grade){
+    if(grade >= 90 && grade <= 100) return 'A'
+    else if(grade >= 80 && grade <= 89) return 'B'
+    else if(grade >= 70 && grade <= 79) return 'C'
+    else if(grade >= 60 && grade <= 69) return 'D'
+    else if( grade <= 60 && grade >= 0) return 'F'
+    else return 'Not Valid Grade.please write a grade more then or equals 0 and less then or equals to 100!'
+}
+console.log(checkStudentGrade(95));
+console.log(checkStudentGrade(75));
+console.log(checkStudentGrade(83));
+console.log(checkStudentGrade(60));
+console.log(checkStudentGrade(110));
+console.log(checkStudentGrade(-20));
