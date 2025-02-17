10f18122c315ab69ff06f443befb1637aecd323e
owias azizi <owaisazizi360@gmail.com>
2025-02-17 15:01:34 +0430

    ATM Withdrawal

--- Codding-Chalange/medium.js
+++ Codding-Chalange/medium.js
@@ -8,12 +8,11 @@
  }
    checkIsValidTriangle(10,20,15)
 
-   //6-Speeding Ticket Calculator
+//6-Speeding Ticket Calculator
 //    Speed limit = 60 km/h.
 //    If a driver goes above the speed limit, they get a fine.
 //    If they are driving over 100 km/h, they get double the fine.
 //    Implement a program that takes speed as input and determines the fine.
-   
    const speedingTicketCalculator = speed =>{
       let fine = 0
       if(speed > 60 && speed <= 100 ) return `You Got ${++fine} fine`
@@ -23,4 +22,20 @@
    console.log(speedingTicketCalculator(90));
    console.log(speedingTicketCalculator(120));
    console.log(speedingTicketCalculator(12));
-   
+
+//  7-  ATM Withdrawal
+//    A person wants to withdraw money from an ATM.
+//    If the withdrawal amount is a multiple of 10, allow withdrawal.
+//    If not, print an error message.
+//    If the account balance is less than the withdrawal amount, print "Insufficient Funds".
+      function withdrawFromATM(withdrawAmount){
+      let balance = 1000
+      if(withdrawAmount % 10 === 0){ 
+        balance =  balance - withdrawAmount
+        console.log(`Your widthdraw was successfull ${withdrawAmount}! Your current balance is ${balance}`);
+     }
+      else if(withdrawAmount % 10 !== 10) console.log('Failed: your withdraw amount is not dividible by 10');
+      else if( withdrawAmount > balance) console.log('Insufficient Funds');
+    }
+     withdrawFromATM(100)
+    
