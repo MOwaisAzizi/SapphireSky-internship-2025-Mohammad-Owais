17104cd55bc692469dfaf8dc2c021ad21ffdc289
owias azizi <owaisazizi360@gmail.com>
2025-02-18 15:53:29 +0430

    multiplication table up to 10

--- Coding-Challange/loop.js
+++ Coding-Challange/loop.js
@@ -14,3 +14,11 @@ function sumOfNumber(number) {
     // return ((number+1)*number)/2
 }
 console.log(sumOfNumber(5));
+
+//Ask the user for a number and print its multiplication table up to 10.
+function MultiplicationTable(number) {
+    for (let i = 1; i <= number; i++) {
+       console.log(`${i} x ${number} = ${i*number}`);
+    }
+}
+MultiplicationTable(10)
