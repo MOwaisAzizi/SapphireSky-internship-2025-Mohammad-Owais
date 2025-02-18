6851b5a6630c32c7c10413bea52c7cc34b0458f3
owias azizi <owaisazizi360@gmail.com>
2025-02-18 16:22:02 +0430

    Count Digits in a Number

--- Coding-Challange/loop.js
+++ Coding-Challange/loop.js
@@ -35,3 +35,14 @@ function reverseNumber(number) {
 }
 
 console.log(reverseNumber(123));
+
+//5- Count Digits in a Number
+   function countDigit(number){
+    let count = 0;    
+     for(let i = 1; i <= String(number).length; i++){
+       count++
+     }
+     return count
+   }
+   console.log(countDigit(3933333));
+   
