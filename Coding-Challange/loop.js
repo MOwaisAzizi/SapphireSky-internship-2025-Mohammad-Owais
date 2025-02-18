fbb887a348f0c2942245ed84c8a0e386c42447d2
owias azizi <owaisazizi360@gmail.com>
2025-02-18 15:48:31 +0430

    Sum of First N Natural Numbers

--- Coding-Challange/loop.js
+++ Coding-Challange/loop.js
@@ -3,3 +3,14 @@ for(let i = 1; i<=100;i++){
     console.log(i);
 }
 
+//2-Sum of First N Natural Numbers
+function sumOfNumber(number){
+    let sum = 0
+    for(let i = 1; i <= number; i++){
+        sum+=i
+}
+return sum
+////or 
+// return ((number+1)*number)/2
+}
+console.log(sumOfNumber(5));
