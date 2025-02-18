c896f52cde4272ea0f10f5c3414f419430800398
owias azizi <owaisazizi360@gmail.com>
2025-02-18 14:35:24 +0430

    Electricity bill calculator

--- Coding-Challange/medium.js
+++ Coding-Challange/medium.js
@@ -67,7 +67,14 @@ const BMICalculator = (weight, height) => {
 // Above 500 units: $1.50 per unit
 // Write a program that calculates the total electricity bill.
 
-
+function billCalculator(unit) {
+    let bill = 0
+    if(unit < 100) return  bill  = unit * 0.5
+    else if (unit <= 201 && unit >= 101 ) return  bill = unit * 0.75 
+    else if(unit < 500 && unit >= 201) return  bill = unit * 1.2 
+    else if(unit > 500) return bill  = unit * 1.5
+}
+ console.log(billCalculator(300));
 
 
 
