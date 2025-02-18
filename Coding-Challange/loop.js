0de74132c982db8aea16118eff23b408dd606af9
owias azizi <owaisazizi360@gmail.com>
2025-02-18 15:48:47 +0430

    format

--- Coding-Challange/loop.js
+++ Coding-Challange/loop.js
@@ -1,16 +1,16 @@
 //1-Print Numbers from 1 to 100
-for(let i = 1; i<=100;i++){
+for (let i = 1; i <= 100; i++) {
     console.log(i);
 }
 
 //2-Sum of First N Natural Numbers
-function sumOfNumber(number){
+function sumOfNumber(number) {
     let sum = 0
-    for(let i = 1; i <= number; i++){
-        sum+=i
-}
-return sum
-////or 
-// return ((number+1)*number)/2
+    for (let i = 1; i <= number; i++) {
+        sum += i
+    }
+    return sum
+    ////or 
+    // return ((number+1)*number)/2
 }
 console.log(sumOfNumber(5));
