618606d0960f1f591ca36b4007bd7f86366234f3
owias azizi <owaisazizi360@gmail.com>
2025-02-19 15:37:09 +0430

    Palindrome Checker

--- Coding-Challange/loop.js
+++ Coding-Challange/loop.js
@@ -106,10 +106,27 @@ findSmallAndLargeNumber([2,3,4,5,1,4,33,1])
         temp = a + b
         a = b
         b = temp
-
     }
     return b
  }
  console.log(findFibonacci(5));
  
+//10-Palindrome Checker
+  function palindromeChecker(number){
+    const numberToArray = String(number).split('')
+    let left = 0 
+    let right = numberToArray.length-1
+
+    while(right > left){
+        if(numberToArray[left] !== numberToArray[right]) {
+          return false
+        }
+        left++;
+        right--;
+    }
+    return true
+  }
+  console.log(palindromeChecker(1221));
+  console.log(palindromeChecker(12231));
+  
 
