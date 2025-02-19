51febee0db9d064142d4ee6156eac4f9bc67f07e
owias azizi <owaisazizi360@gmail.com>
2025-02-19 16:40:57 +0430

    Find GCD (Greatest Common Divisor)

--- Coding-Challange/loop.js
+++ Coding-Challange/loop.js
@@ -168,5 +168,18 @@ function collatzSequence(number){
 }
 console.log(collatzSequence(3));
 
+//13-Find GCD (Greatest Common Divisor)
+ function GCDChecker(a,b){
+    if(a<b) return 'first input must be biger then second'
 
+    while(b!==0){
+        let temp = b
+        b = a % b
+        a = temp
+    }
+    return a
+ }
+ console.log(GCDChecker(60,72));
 
+ 
+ 
