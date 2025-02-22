5cf0f22884c409767171dec72f95773d63423893
owias azizi <owaisazizi360@gmail.com>
2025-02-22 16:33:21 +0430

    Check if a String is a Palindrome

--- Coding-Challange/functions.js
+++ Coding-Challange/functions.js
@@ -92,3 +92,20 @@ console.log(factorial(4));
     }
     console.log(findFibonacci(5));
 
+    //10-Check if a String is a Palindrome
+    function palindromeChecker(str) {
+        const strArray = str.split('')
+        let left = 0
+        let right = strArray.length - 1
+    
+        while (right > left) {
+            if (strArray[left] !== strArray[right]) {
+                return false
+            }
+            left++;
+            right--;
+        }
+        return true
+    }
+    
+    console.log(palindromeChecker('Hih'));
