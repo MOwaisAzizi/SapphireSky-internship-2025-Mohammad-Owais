8fd40f788ab3f9c405161774afd74c8964529c16
owias azizi <owaisazizi360@gmail.com>
2025-02-22 11:23:45 +0430

    Find the Maximum of Three Numbers

--- Coding-Challange/functions.js
+++ Coding-Challange/functions.js
@@ -8,3 +8,13 @@ function celsiusToFahrenheit(celsius){
     return fahrenheit
 }
 console.log(celsiusToFahrenheit(5));
+
+// 2-Find the Maximum of Three Numbers
+ function maxOfThree(a, b, c){
+    if(a > b && a > c) return a
+    else if(b > a && b > c) return b
+    else if(c > a && c > b) return c
+    else if(a === b && b === c) return 'Equal'
+ }
+ console.log(maxOfThree(4,5,4));
+ 
