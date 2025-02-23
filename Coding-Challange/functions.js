3a67f19fabd44df95efa6fb5331d17e1b2b94b79
owias azizi <owaisazizi360@gmail.com>
2025-02-23 14:06:25 +0430

    Find Unique Elements in an Array

--- Coding-Challange/functions.js
+++ Coding-Challange/functions.js
@@ -177,3 +177,17 @@ function bubbleSort(arr) {
 }
 console.log(bubbleSort([8, 4, 5, 6, 1, 21, 1]));
 
+//15-Find Unique Elements in an Array
+function uniqueElements(arr){
+    let arrObj = {}
+    let tempArray = []
+    for(let i = 0;i<arr.length; i++){
+        if(!arrObj[arr[i]]){
+            arrObj[i] = arr[i]
+           tempArray.push(arr[i])
+        }
+    }
+    return tempArray
+}
+console.log(uniqueElements([1,8,2,3,4,2,1,2]));
+
