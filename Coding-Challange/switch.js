af6b5f46fbd5ab0b036409b5e7d9b86cdbc104b6
owias azizi <owaisazizi360@gmail.com>
2025-02-24 11:11:14 +0430

    Month Name and Days

--- Coding-Challange/switch.js
+++ Coding-Challange/switch.js
@@ -45,3 +45,28 @@ function calculator(num1,num2,operator){
     }
     console.log(trafficLightController('Red'));
   
+
+   //4-Month Name and Days
+    function getMonthName(number) {
+        if(typeof number !== 'number') return 'Please Enter a Number!'
+
+        switch (number) {
+            case 1: return 'January';
+            case 2: return 'February';
+            case 3: return 'March';
+            case 4: return 'April';
+            case 5: return 'May';
+            case 6: return 'June';
+            case 7: return 'July';
+            case 8: return 'August';
+            case 9: return 'September';
+            case 10: return 'October';
+            case 11: return 'November';
+            case 12: return 'December';
+            default: return 'Invalid month number [1-12]';
+        }
+    }
+    console.log(getMonthName(2)); 
+
+
+
