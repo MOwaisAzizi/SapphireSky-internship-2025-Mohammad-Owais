f2a17dea7d745169188f4b3e43df53bc58d7bff3
owias azizi <owaisazizi360@gmail.com>
2025-02-24 11:10:53 +0430

    Traffic Light System

--- Coding-Challange/switch.js
+++ Coding-Challange/switch.js
@@ -32,3 +32,16 @@ function calculator(num1,num2,operator){
    console.log(calculator(4, 5, '*'))
 
    
+   //3-Traffic Light System
+  function trafficLightController(color){
+    if(typeof color !== 'string') return 'We expected an String!'
+    color = color.toLocaleLowerCase()
+    switch (color) {
+        case 'red' : return 'Stop'
+        case 'green': return 'Go...'
+        case 'yellow': return 'Slow down'
+        default: return 'Not Valid Color! [red,green,yellow]'
+       }
+    }
+    console.log(trafficLightController('Red'));
+  
