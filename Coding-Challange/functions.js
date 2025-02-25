8e146304f091c97ecc2e4a7a1c8624230a38fca9
Mohammad Owais Azizi <owaisazizi360@gmail.com>
2025-02-25 14:23:43 +0430

    Update Some Text

--- Coding-Challange/functions.js
+++ Coding-Challange/functions.js
@@ -1,8 +1,8 @@
 //EASY PAER
+
 //1- Convert Celsius to Fahrenheit
 // Write a function celsiusToFahrenheit(celsius) that converts a temperature from Celsius to Fahrenheit using the formula:
 //F=(C×95)+32F = (C \times \frac{9}{5}) + 32F=(C×59​)+32
-
 function celsiusToFahrenheit(celsius) {
     const fahrenheit = (celsius * 9 / 5) + 32
     return fahrenheit
