c2fd1e7efd65c93e3e867388dddf8c1a328bd5fa
owias azizi <owaisazizi360@gmail.com>
2025-02-27 16:30:32 +0430

    array contains a specific element

--- array.js
+++ array.js
@@ -13,7 +13,7 @@ function largestElement(arr) {
  }
  console.log(largestElement([1, 2, 3, 6, 3, 2]));
  
- 
+
  //2-Write a function to find the sum of all elements in an array.
 function sumOfArray(arr) {
     let sum = 0
@@ -23,3 +23,16 @@ function sumOfArray(arr) {
     return sum
  }
  console.log(sumOfArray([3, 4, 5]));
+
+ 
+ //3-How do you check if an array contains a specific element?
+function isInArray(arr, element) {
+    for (let i = 0; i < arr.length; i++) {
+       if (arr[i] === element) return true
+    }
+    return false
+    //SECOND WAY
+    // const isElementInArray = arr.includes(element)
+    // return isElementInArray
+ }
+ console.log(isInArray([3, 4, 5, 6], 3));
