b8ef7cf31bce2a390f56b9cd9f6cdf48be7fbab3
owias azizi <owaisazizi360@gmail.com>
2025-03-01 11:21:59 +0430

    delete some extra file

--- Coding-Challange/bonus.js
+++ Coding-Challange/bonus.js
@@ -1,19 +1,17 @@
 // Bonus Challenge: Nested Loops
 // 1-Print Prime Numbers from 1 to 100
 function printPrimeNumbers() {
-    for (let num = 2; num <= 100; num++) { 
+    for (let i = 2; i <= 100; i++) { 
         let isPrime = true; 
-        for (let i = 2; i <= Math.sqrt(num); i++) { 
-            if (num % i === 0) { 
+        for (let j = 2; j <= Math.sqrt(i); j++) { 
+            if (i % j === 0) { 
                 isPrime = false; 
                 break; 
             }
         }
         if (isPrime) {
-            console.log(num); 
+            console.log(i); 
         }
     }
 }
 printPrimeNumbers();
-
-console.log(Math.sqrt(12));
