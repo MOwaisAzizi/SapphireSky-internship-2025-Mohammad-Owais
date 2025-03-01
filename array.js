bf9c4b442b9cdc8c61dc936b62ab6d1be0724bff
owias azizi <owaisazizi360@gmail.com>
2025-03-01 11:02:49 +0430

    update sortArrya

--- array.js
+++ array.js
@@ -79,14 +79,18 @@ console.log(reverseArray([5, 4, 3, 2, 1]));
 //7-How do you merge two sorted arrays into one sorted array?
 function sortedArray(sortedArr1, sortedArr2) {
     let mergedArrays = [...sortedArr1, ...sortedArr2]
-
-    let newSortedArray = []
+   
     for (let i = 0; i < mergedArrays.length; i++) {
-        newSortedArray[i] = mergedArrays[i]
+
+        for (let j = i; j <= mergedArrays.length; j++) {
+            if (mergedArrays[j] > mergedArrays[j + 1]) {
+                [mergedArrays[j], mergedArrays[j + 1] ] = [mergedArrays[j + 1] ,mergedArrays[j]]
+            }
+        }
     }
-    return newSortedArray
+    return mergedArrays
 }
-console.log(sortedArray([1, 2, 3], [4, 5, 6]));
+console.log(sortedArray([1, 3, 5], [2, 4, 6]));//1,2,3,4,5,6
 
 
 //8-Implement a function that rotates an array k times to the right.
@@ -139,4 +143,7 @@ function intersectionArrays(arr1, arr2) {
     newArr = newArr.filter(() => true)
     return newArr
 }
-console.log(intersectionArrays([2, 3, , 84, 5, , 21, 1], [3, 4, 1, 5, 5, 7]));
+console.log(intersectionArrays([2, 3, 84, 5, , 21, 1], [3, 4, 1, 5, 5, 7]));
+
+
+////HARD SECTION
