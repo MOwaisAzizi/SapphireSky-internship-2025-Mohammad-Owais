e021301442fd8ce0b8c1b4812446f05a8ce21bb1
owias azizi <owaisazizi360@gmail.com>
2025-03-01 12:00:52 +0430

    update some text

--- array.js
+++ array.js
@@ -147,3 +147,16 @@ console.log(intersectionArrays([2, 3, 84, 5, , 21, 1], [3, 4, 1, 5, 5, 7]));
 
 
 ////HARD SECTION
+
+function findFibonacci(number) {
+    let temp = 0
+    let a = 0
+    let b = 1
+    for (let i = 1; i < number-1; i++) {
+        temp = a + b
+        a = b
+        b = temp
+    }
+    return b
+}
+console.log(findFibonacci(5));
