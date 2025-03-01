ee4623b8cf7ff73169a38d206824e8d4f184d0f4
Mohammad Owais Azizi <owaisazizi360@gmail.com>
2025-03-01 13:26:02 +0430

    Update StartTriangel

--- Coding-Challange/loop.js
+++ Coding-Challange/loop.js
@@ -169,17 +169,21 @@ console.log(GCDChecker(60, 72));
 //   ***  
 //  *****  
 // *******  
+ function startTrianglePattern(n){
+    for (let i = 1; i < n; i++) {
+        let stars = ''
 
-for (let i = 1; i < 5; i++) {
-    let stars = ''
-    for (let j = 1; j <= i; j++) {
-        if (i === 1 && j === 1) stars += '   '
-        else if (i === 2 && j === 1) stars += '  '
-        else if (i === 3 && j === 1) stars += ' '
-        i === j ? stars += '*' : stars += '**'
+        for (let s = i; s < n-1; s++){
+            stars += ' '
+        }
+    
+        for (let j = 1; j <= i; j++) {
+            i === j ? stars += '*' : stars += '**'
+        }
+        console.log(stars);
     }
-    console.log(stars);
-}
+ }
+startTrianglePattern(10)
 
 //15. Number Guessing Game
 // - The program randomly selects a number between 1 and 100.
