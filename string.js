d531773ea58227f4a460c5cf315ea27c920f5e10
owias azizi <owaisazizi360@gmail.com>
2025-03-02 16:29:27 +0430

    is string Palindrom?

---
+++ string.js
@@ -0,0 +1,27 @@
+
+
+function isPalindrom(str) {
+    ////First Solution
+
+    const arrString = []
+    for (let i = 0; i <= str.length; i++) {
+        arrString[i] = str[i]
+    }
+    arrString.reverse()
+    const newString = arrString.join('')
+    return newString === str
+    
+   ////Second Solution
+
+    // let left = 0
+    // let right = str.length-1
+    // while(left < right){
+    //     if(str[right] === str[left]) {
+    //     left ++
+    //     right --
+    //     } 
+    //     else return false
+    // }
+    //   return true
+    }
+    console.log(isPalindrom('wow'));
