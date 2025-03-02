24df6003219f5146265e28f6bf923e2d34ba6217
owias azizi <owaisazizi360@gmail.com>
2025-03-02 16:38:49 +0430

    count vowels in a string

--- string.js
+++ string.js
@@ -1,8 +1,8 @@
+////Stirng part
 
-
+// 1- is stirng is palindrom?
 function isPalindrom(str) {
     ////First Solution
-
     const arrString = []
     for (let i = 0; i <= str.length; i++) {
         arrString[i] = str[i]
@@ -25,3 +25,16 @@ function isPalindrom(str) {
     //   return true
     }
     console.log(isPalindrom('wow'));
+
+    
+    // 2- count vowels in a string
+    function countVowels(str){
+        const vowels = ['a', 'e', 'i', 'o', 'u']
+        let count = 0
+        for(let i = 0; i<str.length; i++){
+            if(vowels.includes(str[i].toLowerCase())) count ++
+        }
+        return count
+    }
+    console.log(countVowels('Hello World'));
+    
