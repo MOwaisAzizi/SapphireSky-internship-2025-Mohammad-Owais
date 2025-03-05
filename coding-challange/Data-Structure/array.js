0f5ed515971c63fd4a2c07d975aebc2721bef0c5
owias azizi <owaisazizi360@gmail.com>
2025-03-05 14:19:18 +0430

    subarray with the maximum sum(Kadane's Algorithm)

--- coding-challange/Data-Structure/array.js
+++ coding-challange/Data-Structure/array.js
@@ -213,9 +213,21 @@ console.log(areArraysEqual([1, 2, 3], [2,1,3]));
 
 
 //15- implement a function to find the subarray with the maximum sum(Kadane's Algorithm)
- function maxSubArraySum(arr){
+ function maxSubarraySum(arr) {
+    let maxSum = -Infinity;
+    let currentSum = 0;
+
+    for (let num of arr) {
+        currentSum += num;
+        maxSum = Math.max(maxSum, currentSum);
+        if (currentSum < 0) {
+            currentSum = 0;
+        }
+    }
+    return maxSum;
+}
+console.log(maxSubarraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4])); 
  
- }
 
 
 
