5ae6f8403b93b14fcb92982fdb9ce719471113da
owias azizi <owaisazizi360@gmail.com>
2025-03-05 11:58:42 +0430

    Private Properties in JavaScript Classes

--- Coding-Challange/OOP-Debug.js
+++ Coding-Challange/OOP-Debug.js
@@ -13,3 +13,18 @@ class Car {
 const car1 = new Car("Toyota", "Corolla");
 car1.displayInfo();
 
+
+
+//6. Creating Private Properties in JavaScript Classes
+class Employee {
+    #salary;
+    constructor(name, salary) {
+        this.name = name; this.#salary = salary;
+    }
+    getSalary() {
+        return this.#salary;
+    }
+}
+const emp = new Employee("Alice", 50000);
+ console.log(emp.getSalary());
+
