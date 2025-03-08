fe816ffa2641448549b5f9b76cdb768da9e3ac0c
owias azizi <owaisazizi360@gmail.com>
2025-03-08 11:51:12 +0430

    change roman converter to swich case

--- coding-challange/Switch-Bonus-OOP/switch-s2.js
+++ coding-challange/Switch-Bonus-OOP/switch-s2.js
@@ -62,21 +62,19 @@ console.log(rockPaperScissors('rock', 'paper', 'scissors'));
 // Take an integer (1-10) as input.
 // Use switch to print the corresponding Roman numeral.
 function romanConverter(number) {
-    const romanNumbers = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+    if(number > 10 || number < 1) return 'Invalid Month. please Enter Between 1-10'
 
     switch (number) {
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
-        case number: return romanNumbers[number - 1]
+        case 1: return "I"
+        case 2: return "II"
+        case 3: return "III"
+        case 4: return "IV"
+        case 5: return "V"
+        case 6: return "VI"
+        case 7: return "VII"
+        case 8: return "VIII"
+        case 9: return "IX"
+        case 10: return "X"
         default: return 'Invalid Month!'
     }
 }
