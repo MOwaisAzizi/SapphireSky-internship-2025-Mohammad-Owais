95adff4a7a5d8983e1b127fa847d0ff5f763908e
owias azizi <owaisazizi360@gmail.com>
2025-03-09 10:00:06 +0430

    find the length of a linked list

--- linked-list.js
+++ linked-list.js
@@ -13,26 +13,34 @@ class LinkedList {
         this.first = null;
     }
 
-//2-Implement a function to insert a node at the beginning of a linked list.
+    //2-Implement a function to insert a node at the beginning of a linked list.
     addFirst(data) {
         let newNode = new Node(data);
         newNode.next = this.first;
         this.first = newNode;
     }
 
-//3-How do you delete a node from a linked list
-    deleteFrist(){
-       let temp = this.first
-       this.first = this.first.next
-       return temp
+    //3-How do you delete a node from a linked list
+    deleteFrist() {
+        let temp = this.first
+        this.first = this.first.next
+        return temp
     }
 
+    //4-Write a function to traverse and print a linked list.
+    //5-Implement a program to find the length of a linked list.
     display() {
-       let temp = this.first
-       while(temp !== null){
-        console.log(temp.data);
-        temp = temp.next
-       }
+        let temp = this.first
+        let count = 0
+
+        while (temp !== null) {
+            console.log(temp.data);
+            temp = temp.next
+            count++
+        }
+
+        console.log(count);
+
     }
 }
 
