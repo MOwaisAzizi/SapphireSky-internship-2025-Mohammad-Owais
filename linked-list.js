de586443e74543e0a0af122d420d2c9fb98b9ded
owias azizi <owaisazizi360@gmail.com>
2025-03-09 11:50:04 +0430

    reverse a linked list

--- linked-list.js
+++ linked-list.js
@@ -31,29 +31,70 @@ class LinkedList {
     //5-Implement a program to find the length of a linked list.
     display() {
         let temp = this.first
-        let count = 0
 
         while (temp !== null) {
             console.log(temp.data);
             temp = temp.next
-            count++
         }
 
-        console.log(count);
+    }
 
+    countLinkedList(){
+        let temp = this.first
+        let count = 0
+        while (temp !== null) {
+            temp = temp.next
+            count++
+        }
+        return count
+    }
+
+    //MIDIUM QUESTIONS
+
+    // 6-Write a function to find the middle node of a linked list.
+    findMiddleOfList() {
+        let slowStep = this.first
+        let fastStep = this.first
+
+        while (fastStep !== null && fastStep.next !== null) {
+            fastStep = fastStep.next.next
+            slowStep = slowStep.next
+        }
+
+        return slowStep.data
+    }
+
+    //7-Implement a function to reverse a linked list.
+    reverse() {
+        let prev = null;
+        let current = this.first;
+        let next = null;
+
+        while (current !== null) {
+            next = current.next;
+            current.next = prev;  
+            prev = current;      
+            current = next;
+        }
+
+        this.first = prev;  // Update the head to the new first node
     }
 }
 
 const linkedList = new LinkedList()
+linkedList.addFirst(1)
+linkedList.addFirst(2)
 linkedList.addFirst(3)
 linkedList.addFirst(4)
+linkedList.addFirst(5)
+linkedList.addFirst(6)
 linkedList.deleteFrist()
+console.log(linkedList.findMiddleOfList());
 linkedList.display()
+linkedList.reverse()
+linkedList.display()
+console.log(linkedList.countLinkedList());
 
 
-//MIDIUM QUESTIONS
 
-// 6-Write a function to find the middle node of a linked list.
- findMiddleNode(){
-    
- }
+
