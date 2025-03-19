2c106fe15e566a0bb2d86549742aabe6fccfe783
owias azizi <owaisazizi360@gmail.com>
2025-03-19 15:45:35 +0430

    sorting

--- Coding-Challange/LinkedList/isTwoListsIntersects.js
+++ Coding-Challange/LinkedList/isTwoListsIntersects.js
@@ -1,33 +1,33 @@
-    
-    //12- check if two linked lists intersects?
-    function isTwoListsIntersects (list1, list2) {
-        let lengthList1 = list1.sizeOfLinkedList();
-        let lengthList2 = list2.sizeOfLinkedList();
 
-        let d = lengthList1 - lengthList2;
+//12- check if two linked lists intersects?
+function isTwoListsIntersects(list1, list2) {
+    let lengthList1 = list1.sizeOfLinkedList();
+    let lengthList2 = list2.sizeOfLinkedList();
 
-        let current1 = list1.first;
-        let current2 = list2.first;
+    let d = lengthList1 - lengthList2;
 
-        while (d > 0) {
-            current1 = current1.next;
-            d--;
-        }
+    let current1 = list1.first;
+    let current2 = list2.first;
 
-        while (d < 0) {
-            current2 = current2.next;
-            d++;
-        }
-
-        while (current1 && current2) {
-            if (current1 === current2) {
-                return true;
-            }
-            current1 = current1.next;
-            current2 = current2.next;
-        }
-
-        return false;
+    while (d > 0) {
+        current1 = current1.next;
+        d--;
     }
 
-    module.exports = isTwoListsIntersects
+    while (d < 0) {
+        current2 = current2.next;
+        d++;
+    }
+
+    while (current1 && current2) {
+        if (current1 === current2) {
+            return true;
+        }
+        current1 = current1.next;
+        current2 = current2.next;
+    }
+
+    return false;
+}
+
+module.exports = isTwoListsIntersects
