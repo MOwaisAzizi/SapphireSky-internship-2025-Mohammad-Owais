82043fd760e67515b5bdbf7d26bd4a62bfd2035d
owias azizi <owaisazizi360@gmail.com>
2025-03-22 11:08:57 +0430

    function to find the minimum element in a stack in constant time

---
+++ Coding-Challange/Stack/Medium/findMinimumElement.js
@@ -0,0 +1,42 @@
+//9- Implement a function to find the minimum element in a stack in constant time.
+
+class Stack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    // Push an element onto the stack
+    push(element) {
+        this.stack.push(element);
+
+        if (this.minStack.length === 0 || element <= this.minStack[this.minStack.length - 1]) {
+            this.minStack.push(element);
+        } else {
+            this.minStack.push(this.minStack[this.minStack.length - 1]);
+        }
+    }
+
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
+
+// Test
+const stack = new Stack();
+
+stack.push(3);
+stack.push(2);
+stack.push(1);
+stack.push(4);
+
+console.log(stack.getMin());
+stack.pop();
+console.log(stack);
+
+console.log(stack.getMin());
--- Coding-Challange/Stack/Medium/postfix.js
+++ Coding-Challange/Stack/Medium/postfix.js
@@ -2,31 +2,31 @@
 
 const StackArray = require("../Easy/StackArray");
 
-function evaluatePostfix(arr){
-   let stackArray = new StackArray()
+function evaluatePostfix(arr) {
+    let stackArray = new StackArray()
 
-   for (let element of arr){
-    if(!isNaN(element)){
-        stackArray.push(element)
-    }else{
-        let num1 = stackArray.pop()
-        let num2 = stackArray.pop()
+    for (let element of arr) {
+        if (!isNaN(element)) {
+            stackArray.push(element)
+        } else {
+            let num1 = stackArray.pop()
+            let num2 = stackArray.pop()
 
-        switch(element){
-          case '+' : stackArray.push(num1 + num2)
-           break
-           case '-' : stackArray.push(num1 - num2)
-           break
-           case '*' : stackArray.push(num1 * num2)
-           break
-           case '/' : stackArray.push(num1 / num2)
-           break
+            switch (element) {
+                case '+': stackArray.push(num1 + num2)
+                    break
+                case '-': stackArray.push(num1 - num2)
+                    break
+                case '*': stackArray.push(num1 * num2)
+                    break
+                case '/': stackArray.push(num1 / num2)
+                    break
+            }
         }
     }
-   }
-   return stackArray.pop()
+    return stackArray.pop()
 }
 
 //testing
-console.log(evaluatePostfix([2,3,'+',3,'-']));
-console.log(evaluatePostfix([2,3,'+',5,'*']));
+console.log(evaluatePostfix([2, 3, '+', 3, '-']));
+console.log(evaluatePostfix([2, 3, '+', 5, '*']));
