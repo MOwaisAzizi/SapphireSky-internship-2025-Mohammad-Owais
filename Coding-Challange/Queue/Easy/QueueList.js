68cff7f94c3451f1e50c03b8eb0c916c66e57048
owias azizi <owaisazizi360@gmail.com>
2025-03-23 15:08:55 +0430

    How do you implement a queue using a linked list?

---
+++ Coding-Challange/Queue/Easy/QueueList.js
@@ -0,0 +1,56 @@
+const Node = require('./Node')
+
+//3- How do you implement a queue using a linked list?
+class QueueList {
+    constructor() {
+        this.front = null;
+        this.rear = null;
+    }
+
+    enqueue(data) {
+        const newNode = new Node(data);
+
+        if (this.front === null) { 
+            this.front = newNode;
+            this.rear = newNode;
+        } else {
+            this.rear.next = newNode; 
+            this.rear = newNode; 
+        }
+    }
+
+    dequeue() {
+        if (this.front === null) {
+            console.log("Queue is empty!");
+            return null;
+        }
+
+        const deletedData = this.front.data;
+        this.front = this.front.next;
+
+        if (this.front === null) { 
+            this.rear = null; 
+        }
+
+        return deletedData;
+    }
+
+    isEmpty() {
+        return this.front === null;
+    }
+
+    peek() {
+        return this.front ? this.front.data : null;
+    }
+}
+
+// Testing the queue
+const queue = new QueueList();
+queue.enqueue(4);
+queue.enqueue(3);
+queue.enqueue(2);
+queue.enqueue(5);
+
+console.log(queue.dequeue()); // 4
+console.log(queue.dequeue()); // 3
+console.log(queue);
