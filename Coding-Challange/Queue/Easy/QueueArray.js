6b9ae55bb1edba85b2d2b8119254f912d2c67f7c
owias azizi <owaisazizi360@gmail.com>
2025-03-23 14:48:24 +0430

    a queue using an array in JavaScript.

---
+++ Coding-Challange/Queue/Easy/QueueArray.js
@@ -0,0 +1,48 @@
+//2- Implement a queue using an array in JavaScript.
+class QueueArray {
+    constructor() {
+        this.queue = [];
+        this.front = -1;
+        this.rear = -1;
+    }
+
+    enqueue(element) {
+        if (this.front === -1) {
+            this.front = 0;
+        }
+        this.rear++;
+        this.queue[this.rear] = element;
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            console.log("Queue is empty!");
+            return null;
+        }
+
+        const deletedElement = this.queue[this.front];
+        this.front++;
+
+        if (this.front > this.rear) {
+            this.front = this.rear = -1;
+        }
+
+        return deletedElement;
+    }
+
+    isEmpty() {
+        return this.front === -1;
+    }
+
+    peek() {
+        return this.isEmpty() ? null : this.queue[this.front];
+    }
+}
+
+
+const queue = new QueueArray()
+queue.enqueue(4)
+queue.enqueue(3)
+queue.enqueue(2)
+console.log(queue.dequeue());
+console.log(queue.peek());
