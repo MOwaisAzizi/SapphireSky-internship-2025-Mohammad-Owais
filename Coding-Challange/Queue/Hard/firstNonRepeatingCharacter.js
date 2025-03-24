51d52caedb05562e141e3f9a338d5ae9737d1188
owias azizi <owaisazizi360@gmail.com>
2025-03-24 16:30:48 +0430

    find the first non-repeating character in a stream of characters using a queue.

--- Coding-Challange/Queue/Hard/firstNonRepeatingCharacter.js
+++ Coding-Challange/Queue/Hard/firstNonRepeatingCharacter.js
@@ -1,5 +1,21 @@
 //12-Implement a function to find the first non-repeating character in a stream of characters using a queue.
 
-function firstNonRepeatingCharacter(){
-    
+function firstNonRepeatingCharacter(stream) {
+    const countMap = {};
+    const queue = [];
+
+    for (let ch of stream) {
+        countMap[ch] = (countMap[ch] || 0) + 1;
+
+        queue.push(ch);
+
+        while (queue.length > 0 && countMap[queue[0]] > 1) {
+            queue.shift();
+        }
+    }
+
+    return queue[0] || 'No Non-Repeating Char'
 }
+
+console.log(firstNonRepeatingCharacter('aabbcc'));
+
