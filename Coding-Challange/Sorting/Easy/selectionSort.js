44bcfb79e247b189c55d65e9ce5897340d16222a
owias azizi <owaisazizi360@gmail.com>
2025-03-25 21:20:52 +0430

    Implement the Insertion Sort algorithm

--- Coding-Challange/Sorting/Easy/selectionSort.js
+++ Coding-Challange/Sorting/Easy/selectionSort.js
@@ -2,15 +2,15 @@
 
 function selectionSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
-    
+
         let min_idx = i;
-        
+
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[min_idx]) {
                 min_idx = j;
             }
         }
-        
+
         let temp = arr[i];
         arr[i] = arr[min_idx];
         arr[min_idx] = temp;
@@ -19,5 +19,5 @@ function selectionSort(arr) {
 }
 
 //Test
-console.log(selectionSort([5,1,3,7]));
-console.log(selectionSort([8,1,3,1]));
+console.log(selectionSort([5, 1, 3, 7]));
+console.log(selectionSort([8, 1, 3, 1]));
