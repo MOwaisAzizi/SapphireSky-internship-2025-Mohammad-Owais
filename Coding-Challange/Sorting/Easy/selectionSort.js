16afe102336a9b95ec4119e3481a3078fc2e9d02
owias azizi <owaisazizi360@gmail.com>
2025-03-25 20:47:52 +0430

    How does Selection Sort work? Implement it

---
+++ Coding-Challange/Sorting/Easy/selectionSort.js
@@ -0,0 +1,23 @@
+//2- How does Selection Sort work? Implement it.
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+    
+        let min_idx = i;
+        
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        
+        let temp = arr[i];
+        arr[i] = arr[min_idx];
+        arr[min_idx] = temp;
+    }
+    return arr
+}
+
+//Test
+console.log(selectionSort([5,1,3,7]));
+console.log(selectionSort([8,1,3,1]));
