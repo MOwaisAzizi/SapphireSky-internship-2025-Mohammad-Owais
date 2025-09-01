cee609bba8f9826654ce3e38ec9fdef4eea50fb1
owias azizi <owaisazizi360@gmail.com>
2025-09-01 23:25:58 +0430

    kth element in array

--- Coding-Challange/Sorting/Medium/kthElementOfArray.js
+++ Coding-Challange/Sorting/Medium/kthElementOfArray.js
@@ -1,8 +1,7 @@
-const countSort = require('./countSort')
 //5- Implement a function to find the Kth largest element in an array using sorting.
 function kthElementOfArray(arr, k) {
-    countSort(arr)
-    return arr[k - 1]
+      const sortedArr = arr.slice().sort((a, b) => b - a);
+    return sortedArr[k - 1]
 }
 //Test
 console.log(kthElementOfArray(([2, 3, 1, 5, 8, 1]), 3));
