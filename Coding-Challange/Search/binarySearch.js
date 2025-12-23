66e8565fa3ec4696aa50c492963130dea46da9df
owias azizi <owaisazizi360@gmail.com>
2025-12-23 22:44:00 +0430

    sorting search file

--- Coding-Challange/Search/binarySearch.js
+++ Coding-Challange/Search/binarySearch.js
@@ -2,15 +2,15 @@
 const binarySearch = (arr, target) => {
     let left = 0;
     let right = arr.length - 1;
-    
-    while(left <= right){
+
+    while (left <= right) {
         let mid = Math.floor((left + right) / 2);
-        if(arr[mid] === target) return mid;
-        else if(arr[mid] > target) right = mid - 1;
-        else if(arr[mid] < target) left = mid + 1
+        if (arr[mid] === target) return mid;
+        else if (arr[mid] > target) right = mid - 1;
+        else if (arr[mid] < target) left = mid + 1
     }
-     return -1
+    return -1
 }
 
-console.log(binarySearch([1,2,3,4,5,6,7,8,9], 7));
-console.log(binarySearch([10,31,44,55,66,77,88,96], 96));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 7));
+console.log(binarySearch([10, 31, 44, 55, 66, 77, 88, 96], 96));
--- Coding-Challange/Search/checkNumberInMatrix.js
+++ Coding-Challange/Search/checkNumberInMatrix.js
@@ -1,16 +1,16 @@
 //5- Write a function to check if a given number exists in a matrix.
-function checkNumberInMatrix(matrix, target){
-for(let i = 0; i < matrix.length; i++){
-    for(let j = 0; j < matrix[i].length; j++){
-        if(matrix[i][j] === target) return true;
+function checkNumberInMatrix(matrix, target) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === target) return true;
+        }
     }
-}
-return false
+    return false
 }
 const matrix = [
-    [3,4,5],
-    [6,5,3],
-    [1,2,8],
+    [3, 4, 5],
+    [6, 5, 3],
+    [1, 2, 8],
 ]
 
 console.log(checkNumberInMatrix(matrix, 2));
